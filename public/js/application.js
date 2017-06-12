78ac4fac46545154799118a5ed67820eb69735ca
Samantha T Cooks <samtcooks@gmail.com>
2017-06-12 18:36:02 -0400

    Create an alert for when a player wins

--- public/js/application.js
+++ public/js/application.js
@@ -17,29 +17,20 @@ $(document).ready(function() {
     if(leftPressed){
       var nextA = $("#player-a").next();
       $("#player-a").insertAfter(nextA)
+        if ($("#playera_strip").children("td").last().attr('class') === "active") {
+          alert("Player A Wins!!")
+        }
     } else if (rightPressed) {
       var nextB = $("#player-b").next();
       $("#player-b").insertAfter(nextB)
+        if ($("#playerb_strip").children("td").last().attr('class') === "active") {
+          alert("Player B Wins!!")
+        }
     }
   });
 
   $(document).on("click", "#restart", function() {
     location.reload(true);
   });
-
-  $(document).on('keyup', "#playera_strip td", function(event) {
-    debugger
-    if ($(this).length === 42){
-      alert('Player A wins!!')
-    }
-  });
-
-
-  $(document).on('keyup', "#playerb_strip td", function(event) {
-    if ($(this).length === 42){
-      alert('Player B wins!!')
-    }
-  });
-
 
 });
